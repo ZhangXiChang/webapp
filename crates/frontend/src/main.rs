5878a97619d93845b4276778d11f21f9d432f44c
ZhangXiChang <yxrbeigua@outlook.com>
2023-09-29 17:24:27 +0800

    提供TailwindCSS支持，现在可以使用TailwindCSS编写CSS。

--- crates/frontend/src/main.rs
+++ crates/frontend/src/main.rs
@@ -1,7 +1,9 @@
+use gloo::*;
 use yew::prelude::*;
 
 fn main() {
     if cfg!(target_family = "wasm") {
+        utils::document().set_title("暴虐仙女的个人小站");
         yew::Renderer::<HTMLBody>::new().render();
     } else if cfg!(not(target_family = "wasm")) {
         println!("This is a front-end application, please compile to 'wasm'!");
@@ -26,7 +28,14 @@ impl yew::Component for RootLayout {
     fn view(&self, _: &Context<Self>) -> Html {
         html! {
             <>
-                <p>{"Hello world!"}</p>
+                <div class="container mx-auto max-w-full rounded bg-white py-3 shadow-md shadow-gray-200">
+                    <div class="mx-96 rounded bg-gray-100 py-5">
+                        <form></form>
+                    </div>
+                    <div class="mx-96 rounded bg-gray-950 py-5">
+                        <form></form>
+                    </div>
+                </div>
             </>
         }
     }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './crates/frontend/src/**/*.rs',
+        './html/index.html'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
